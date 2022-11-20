7ab0e798670666c4e382e2a192fc09c41ddacd42
eversolano <ever10solano@hotmail.com>
2022-11-19 19:01:17 -0600

    change

--- business/ObraImagenesAction.php
+++ business/ObraImagenesAction.php
@@ -4,31 +4,27 @@ include './ObraImagenesBusiness.php';
 
 if(isset($_POST['create'])){
     if(isset($_FILES['obraimagen']) && isset($_POST['obraid'])){
+
         $obraId = $_POST['obraid'];
         $fichero = $_FILES["obraimagen"];
-        $directorio = "../cotizacionimages/";
-        $nombreArchivo = $_FILES["obraimagen"]["name"];
-        $tipoArchivo = $_FILES["obraimagen"]["type"];
-        if($tipoArchivo == "image/png"){
-        $tipoArchivo = "png";
-        }else
-        if($tipoArchivo == "image/jpeg"){
-        $tipoArchivo = "jpeg";
-        }else
-        if($tipoArchivo == "application/pdf"){
-        $tipoArchivo = "pdf";
+        $img = '';
+
+        foreach($fichero["tmp_name"] as $key => $image){
+            $nombreArchivo = $_FILES["obraimagen"]["name"][$key];
+            $tipoArchivo = $_FILES["obraimagen"]["type"][$key];
+            $tmpArchivo = $_FILES["obraimagen"]["tmp_name"][$key];
+            $directorio = "../cotizacionimages/";
+
+            move_uploaded_file($tmpArchivo, "../cotizacionimages/".$nombreArchivo);
+            $ruta = $directorio.$nombreArchivo.".".$tipoArchivo;
+            $ObraImagen = new ObraImagenes(0, $obraId, $directorio.$ruta);
+            $ObraImagenBusiness = new ObraImagenesBusiness();
+            $result = $ObraImagenBusiness->insertCotizacionImagen($ObraImagen);
+
         }
-        $ObraImagenId = isset($_POST['obraimagenid']);
-        $nombre = $ObraImagenId."-".$obraId;
 
-        move_uploaded_file($fichero["tmp_name"], "../cotizacionimages/".$fichero["name"]);
-        rename ($directorio.$nombreArchivo, $directorio.$nombre.".".$tipoArchivo);
-        $ruta = $directorio.$nombre.".".$tipoArchivo;
-
-        $ObraImagen = new ObraImagenes(0, $obraId, $directorio.$ruta);
-        $ObraImagenBusiness = new ObraImagenesBusiness();
-        $result = $ObraImagenBusiness->insertCotizacionImagen($ObraImagen);
-
+        //$ruta = $directorio.$nombre.".".$tipoArchivo;
+        //rename ($directorio.$nombreArchivo, $directorio.$nombre.".".$tipoArchivo);
         if ($result == 1) {
             header("location: ../view/ObraImagenesView.php?success=inserted");
         } else {
